14000f2f31d87d49a61e0a63e4329874d87bd48b
Cameron Soudbash <csoudbash@gmail.com>
2022-01-09 15:27:46 -0600

    finished, started stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = ['celery', 'peanutbutter']
+const maxItems = 5;
+
+
+function addItem(item) { //add item function
+  basket.push(item)
+  return true;
+}
+console.log("Before adding item:" + basket.length);
+console.log(addItem('fish'));
+console.log("the basket is now:", basket);
+console.log("After adding item:" + basket.length);
+//end of add item
+
+//start of listItems
+function listItems() {
+  console.log("Inside listItems():" + basket.length);
+  for (let i = 0; i < basket.length; i++){
+    console.log((basket[i]));
+  }
+  return;
+}
+console.log(listItems());
+//end of listItems
+
+
+function isFull() {
+
+}
